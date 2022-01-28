d84478ff95e3c268afc59b2e032d1cbe982c55de
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-01-28 15:44:30 +0100

    add launch file

---
+++ launch/jetracer.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+import launch_ros.actions import Node
+
+def generate_launch_description():
+    controller_node = Node(
+        package="jetracer_py",
+        node_executable="controller",
+        output="screen",
+        emulate_tty=True
+    )
+    teleop_node = Node(
+        package="jetracer_py",
+        node_executable="teleop",
+        output="screen",
+        emulate_tty=True
+    )
+
+    return LaunchDescription([
+        controller_node,
+        teleop_node
+    ])
